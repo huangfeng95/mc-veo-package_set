12766663d9a0363e6be67bd8bc7196df4133b402
huangfeng <553510599@qq.com>
2023-05-23 15:35:47 +0800

    Add files via upload

---
+++ init.rb
@@ -0,0 +1,20 @@
+# Orocos Specific ignore rules
+#
+#
+#Autoproj.env_set 'MC-VEO_BRANCH', 'master'
+
+configuration_option('MC-VEO_BRANCH', 'string',
+    :default => 'master',
+    :possible_values => ['ceres', 'master'],
+    :doc => [
+        "Which flavor of MC-VEO do you want to use ?",
+        "Stay with the default ('master') if you want to use MC-VEO with DSO backend",
+        "otherwise select ('ceres')"])
+
+Autoproj.config.get('MC-VEO_BRANCH')
+#Autoproj.env_set 'MC-VEO_BRANCH', Autoproj.config.get('MC-VEO_BRANCH')
+#Autoproj.config.set 'MC-VEO_BRANCH', Autoproj.env['MC-VEO_BRANCH']
+
+#pp "SELECTED MC-VEO: " + Autoproj.env['MC-VEO_BRANCH']
+
+
